a8d34ac135b6d0f22d6477e6452de4f8976fede4
zervis <zervis@gmail.com>
2022-12-06 13:56:15 +0100

    Update user.ex

--- lib/rewardly/users/user.ex
+++ lib/rewardly/users/user.ex
@@ -9,7 +9,7 @@ defmodule Rewardly.Users.User do
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
-    field :credits, :integer
+    field :credits, :integer, default: 50
     field :role, :string, default: "member"
     has_many :rewards, Rewardly.Rewards.Reward
 
