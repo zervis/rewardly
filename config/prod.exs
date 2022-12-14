1532575096ca2dd2e162c6d7c3af5638d94d440c
zervis <zervis@gmail.com>
2022-12-14 20:06:31 +0100

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -55,3 +55,9 @@ secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+
+config :rewardly, Rewardly.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
