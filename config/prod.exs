51a4bb502e96af60e08709828c4d49c33ee57b60
zervis <zervis@gmail.com>
2022-12-14 18:24:03 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -36,7 +36,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-#     config :rewardly, RewardlyWeb.Endpoint,
-#       force_ssl: [hsts: true]
+    config :rewardly, RewardlyWeb.Endpoint,
+          force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
