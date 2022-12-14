0aef5244bef438b2389853a6037e8ae5e0825fdc
zervis <zervis@gmail.com>
2022-12-14 20:05:16 +0100

    prod

--- config/prod.exs
+++ config/prod.exs
@@ -21,8 +21,11 @@ config :logger, level: :info
 #
 
 config :rewardly, RewardlyWeb.Endpoint,
+load_from_system_env: true,
 url: [scheme: "https", host: "rewardly.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
+cache_static_manifest: "priv/static/cache_manifest.json",
+secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 #     config :rewardly, RewardlyWeb.Endpoint,
 #       ...,
